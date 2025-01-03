ddcbe41e4723e5b90d8faf184f7f5b457f901a31
deniskt <rearden97@gmail.com>
2025-01-03 20:29:08 +0700

    Delivers data and response on successful HTTP response

--- Tests/HTTPClientTests/URLSessionHTTPClientTests.swift
+++ Tests/HTTPClientTests/URLSessionHTTPClientTests.swift
@@ -91,6 +91,16 @@ final class URLSessionHTTPClientTests: XCTestCase {
 		XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
 	}
 	
+	func test_performRequest_succeedsOnHTTPURLResponseWithData() {
+		let data = anyData()
+		let response = anyHTTPURLResponse()
+		let receivedValues = resultValuesFor(data: data, response: response, error: nil)
+
+		XCTAssertEqual(receivedValues?.data, data)
+		XCTAssertEqual(receivedValues?.response.url, response.url)
+		XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT() -> URLSessionHTTPClient {
